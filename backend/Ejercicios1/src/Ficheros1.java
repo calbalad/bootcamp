d6ec83d58c9e5714298c92e2f4718ef111c8b104
calbalad <christian.albaladejo-carrasco@capgemini.com>
2022-02-09 17:38:16 +0100

    Comprimir

--- backend/Ejercicios1/src/Ficheros1.java
+++ backend/Ejercicios1/src/Ficheros1.java
@@ -1,14 +1,25 @@
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.print.attribute.standard.Compression;
 
 public class Ficheros1 {
-	public static void main(String[] args) {
+	private static final String[] FILES = null;
+
+	public static void main(String[] args) throws Exception {
 		leeFichero();
 		creaFichero();
+		compress("C:\\Eclipse\\curso\\backend\\Ejercicios1\\src\\Salida.txt");
 	}
 
 	public static void leeFichero() {
@@ -35,7 +46,7 @@ public class Ficheros1 {
 	}
 
 	public static void escribir(String cadena, String resultado) {
-		File log = new File("C:\\\\Eclipse\\\\curso\\\\backend\\\\Ejercicios1\\\\src\\\\Salida.txt");
+		File log = new File("C:\\Eclipse\\curso\\backend\\Ejercicios1\\src\\Salida.txt");
 		try{
 		    if(!log.exists()){
 		        System.out.println("We had to make a new file.");
@@ -55,5 +66,23 @@ public class Ficheros1 {
 		    System.out.println("COULD NOT LOG!!");
 		}
 	}
+	
+	 public static void compress(String file) throws Exception {
+		 String sourceFile = file;
+	        FileOutputStream fos = new FileOutputStream("C:\\\\Eclipse\\\\curso\\\\backend\\\\Ejercicios1\\\\src\\\\compressed.zip");
+	        ZipOutputStream zipOut = new ZipOutputStream(fos);
+	        File fileToZip = new File(sourceFile);
+	        FileInputStream fis = new FileInputStream(fileToZip);
+	        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+	        zipOut.putNextEntry(zipEntry);
+	        byte[] bytes = new byte[1024];
+	        int length;
+	        while((length = fis.read(bytes)) >= 0) {
+	            zipOut.write(bytes, 0, length);
+	        }
+	        zipOut.close();
+	        fis.close();
+	        fos.close();
+	    }
 
 }
