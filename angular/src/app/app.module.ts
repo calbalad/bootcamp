5d3f30274867b161482daada2aa4a3c3ffc4fdfd
calbalad <christian.albaladejo-carrasco@capgemini.com>
2022-03-22 16:18:02 +0100

    logger service

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { LoggerService, MyCoreModule } from 'src/lib/my-core';
+import { environment } from 'src/environments/environment';
+import { ERROR_LEVEL, LoggerService, MyCoreModule } from 'src/lib/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SecurityModule } from './security';
     BrowserModule, FormsModule,
     AppRoutingModule, MainModule, SecurityModule, MyCoreModule
   ],
-  providers: [LoggerService,],
+  providers: [LoggerService,
+  {provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
