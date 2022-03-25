a14a6183c9647aa999e0a2d1c5653b6a0b7bdeae
calbalad <christian.albaladejo-carrasco@capgemini.com>
2022-03-25 17:20:03 +0100

    RxJS

--- angular/src/app/demos/demos.component.ts
+++ angular/src/app/demos/demos.component.ts
@@ -5,49 +5,51 @@ import { NotificationService, NotificationType } from '../common-services';
 @Component({
   selector: 'app-demos',
   templateUrl: './demos.component.html',
-  styleUrls: ['./demos.component.css']
+  styleUrls: ['./demos.component.css'],
 })
 export class DemosComponent implements OnInit, OnDestroy {
-  private nombre: string = 'mundo'
+  private nombre: string = 'mundo';
   public listado = [
-    { id: 1, nombre: 'Madrid'},
-    { id: 2, nombre: 'barcelona'},
-    { id: 3, nombre: 'MURCIA'},
-    { id: 4, nombre: 'ciudad Real'},
-  ]
-  idProvincia = 3
+    { id: 1, nombre: 'Madrid' },
+    { id: 2, nombre: 'barcelona' },
+    { id: 3, nombre: 'MURCIA' },
+    { id: 4, nombre: 'ciudad Real' },
+  ];
+  idProvincia = 3;
 
   public resultado: string | null = null;
 
   visible = true;
-  estetica = { importante: true, error: false, urgente: true }
+  estetica = { importante: true, error: false, urgente: true };
 
   fontSize = 24;
 
-  constructor(public vm: NotificationService) { }
+  constructor(public vm: NotificationService) {}
 
-  get Nombre(): string { return this.nombre; }
+  get Nombre(): string {
+    return this.nombre;
+  }
   set Nombre(value: string) {
-    if(value === this.nombre) return;
-    this.nombre = value
+    if (value === this.nombre) return;
+    this.nombre = value;
   }
 
   public saluda(): void {
-    this.resultado = `Hola ${this.Nombre}`
+    this.resultado = `Hola ${this.Nombre}`;
   }
 
   public despide(): void {
-    this.resultado = `Adios ${this.Nombre}`
+    this.resultado = `Adios ${this.Nombre}`;
   }
 
   public di(algo: string): void {
-    this.resultado = `Dice ${algo}`
+    this.resultado = `Dice ${algo}`;
   }
 
   cambia() {
-    this.visible = !this.visible
-    this.estetica.importante = !this.estetica.importante
-    this.estetica.error = !this.estetica.error
+    this.visible = !this.visible;
+    this.estetica.importante = !this.estetica.importante;
+    this.estetica.error = !this.estetica.error;
   }
 
   calcula(a: number, b: number): number {
@@ -55,18 +57,20 @@ export class DemosComponent implements OnInit, OnDestroy {
   }
 
   add(provincia: string) {
-    const id = this.listado.length + 1
-    this.listado.push({id, nombre: provincia})
-    this.idProvincia = id
+    const id = this.listado.length + 1;
+    this.listado.push({ id, nombre: provincia });
+    this.idProvincia = id;
   }
 
   private suscriptor: Unsubscribable | undefined;
   ngOnInit(): void {
-    // this.suscriptor = this.vm.Notificacion.subscribe(n => {
-    //   if (n.Type !== NotificationType.error) { return; }
-    //   window.alert(`Suscripcion: ${n.Message}`);
-    //   this.vm.remove(this.vm.Listado.length - 1);
-    // });
+    this.suscriptor = this.vm.Notificacion.subscribe((n) => {
+      if (n.Type !== NotificationType.error) {
+        return;
+      }
+      window.alert(`Suscripcion: ${n.Message}`);
+      this.vm.remove(this.vm.Listado.length - 1);
+    });
   }
   ngOnDestroy(): void {
     if (this.suscriptor) {
